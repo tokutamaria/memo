625e6ce3c8df0ef5bf9217f0a86465650ed6a659
TAKUTO ARIMA <ta910arima@gmail.com>
2020-08-20 16:05:58 +0900

    DB接続OK

---
+++ index2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+<?php
+try {
+    $db = new PDO('mysql:dbname=mysql;host=127.0.0.1;charset=utf8', 'root','root');
+}catch(PDOException $e) {
+    echo 'DB接続エラー: ' . $e->getMessage();
+}
+?>
+</pre>
+</main>
+</body>
+</html>
