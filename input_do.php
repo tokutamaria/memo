2800fd7d66553542b6616724f75bc4b473edd290
TAKUTO ARIMA <ta910arima@gmail.com>
2020-08-20 23:48:12 +0900

    インプット完成

--- input_do.php
+++ input_do.php
@@ -1,63 +1,35 @@
 <!doctype html>
-
 <html lang="ja">
-
 <head>
-
 <!-- Required meta tags -->
-
 <meta charset="utf-8">
-
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Bootstrap CSS -->
-
 <link rel="stylesheet" href="css/style.css">
 
 <title>PHP</title>
-
 </head>
-
 <body>
-
 <header>
-
 <h1 class="font-weight-normal">PHP</h1>
-
 </header>
 
 <main>
-
 <h2>Practice</h2>
-
 <pre>
-
+<!-- ここにプログラムを記述します -->
 <?php
+    try {
+        $db = new PDO('mysql:dbname=mydb;localhost=3306;charset=utf8','root','root');
 
-try {
-
-    $db = new PDO('mysql:dbname=mydb
-    ;host=localhost;port=3306;charset=utf8','root','root');
-
-} catch(PDOException $e) {
-
-    echo 'DB接続エラー: ' . $e->getMessage();
-
-}
-
-
-$records = $db->query('SELECT * FROM my_items');
-while ($record = $records->fetch()) {
-    print($record['item_name'] . "\n");
-}
-
-
-?>
+       $db -> exec('INSERT INTO memos SET memo="' . $_POST['memo'] .  '", created_at=NOW()');
+    } catch(PDOException $e) {
+        echo 'DB接続エラー:' . $e->getMessage();
+    }
+    ?>
 
 </pre>
-
 </main>
-
 </body>
-
 </html>
